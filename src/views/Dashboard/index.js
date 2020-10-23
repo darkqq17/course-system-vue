bea710efbe7b6177d00903a52d4fe829125bf9dd
Chen-Yi Hsu <11488168+cyihsu@users.noreply.github.com>
2020-10-23 18:35:48 +0800

    feat: sync course list with NCU

--- src/views/Dashboard/index.js
+++ src/views/Dashboard/index.js
@@ -1,10 +1,33 @@
 import React from "react";
+import Nav from "../../components/Nav";
+import courses from "../../constants/courses.json";
 
 function Dashboard() {
+  console.log(courses.length);
   return (
-    <div className="dashboard">
-        <h1>this is dash</h1>
-    </div>
+    <React.Fragment>
+      <Nav />
+      <div className="dashboard container-lg" style={{paddingTop: '150px'}}>
+        <h1>課程清單</h1>
+        <div className="row row-cols-5">
+        {courses.map((course) => (
+          <div className="card m-3 col rounded-lg">
+            <div className="card-body">
+              <p className="card-title">{`${course.no}-${course.classNo}`}</p>
+              <h5 className="card-title">{`${course.name}`}</h5>
+              {
+                course.teachers.map((teacher, index) => {
+                  return(
+                    <span>{(index === 0 ? "" : "、") + teacher}</span>
+                  )
+                })
+              }
+            </div>
+          </div>
+        ))}
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
